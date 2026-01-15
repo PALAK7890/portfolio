8244eb60085178ee1852001f6b34619f3056cb78
Palak <palakdahiya78@gmail.com>
2026-01-15 11:41:20 +0530

    added photo

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,11 +1,22 @@
+import face from "../assets/p.jpeg"
+import "../style/about.css"
+
 export default function About(){
   return (
-    <div className="page">
-      <h1>About Me</h1>
-      <p>
-        Hi, I’m Palak — a Computer Science student who loves Machine Learning,
-        UI/UX and building creative digital experiences.
-      </p>
+    <div className="about-page">
+      <div className="about-left">
+        <img src={face} alt="Palak" />
+      </div>
+
+      <div className="about-right">
+   <p className="about-text">
+  {"Hi, I’m Palak — a passionate Computer Science student who loves Machine Learning, creative UI design, and building beautiful digital experiences. I enjoy blending logic with aesthetics to create products that are both intelligent and visually powerful. I’m always curious, always learning, and always trying to push my creativity to the next level."
+    .split("")
+    .map((char, i) => (
+      <span key={i}>{char === " " ? "\u00A0" : char}</span>
+    ))}
+</p>
+      </div>
     </div>
   )
 }
