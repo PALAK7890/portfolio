156d3c10d3b27aeae582429cbfbf5882f0c59c40
Palak <palakdahiya78@gmail.com>
2026-01-17 20:45:00 +0530

    completes 99%

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,34 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import NavBar from "./components/NavBar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Project";
+import Contact from "./components/Contact";
+import Footer from "./components/footer";
 
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Project"
-import Contact from "./components/Contact"
-import Footer from "./components/footer"
 
 export default function App(){
+
+
   return(
-    <Router>
+    <>
+     <div id="comic-emojis"></div>
 
-      {/* ✈️ COMIC PAPER PLANE BACKGROUND */}
-      <div className="paper-plane-bg">
-        {Array.from({ length: 12 }).map((_, i) => (
-          <span key={i} className="paper-plane">heyyy</span>
-        ))}
-      </div>
+      <Router>
+        <NavBar />
 
-      <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
-      <Footer/>
-
-    </Router>
-  )
+        <Footer/>
+      </Router>
+    </>
+  );
 }
